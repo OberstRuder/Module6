182198f82f7188161f7411c00a5e0641227312c9
Бабко Евгений <doxis32123@gmail.com>
2023-04-01 16:45:19 +0300

    rework_hw1

--- HW1/Models/Farm.cs
+++ HW1/Models/Farm.cs
@@ -1,10 +1,10 @@
 ﻿namespace HW1.Models
 {
-    public class MyTask
+    public class Farm
     {
         public int Id { get; set; }
+        public string KindOfAnimal { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
-        public bool IsComplete { get; set; }
+        public double Weight { get; set; }
     }
 }
--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,3 +1,5 @@
+using HW1.Services;
+
 namespace HW1
 {
     public class Program
@@ -6,26 +8,14 @@ namespace HW1
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
+            builder.Services.AddSingleton<IFarmRepository, FarmRepository>();
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
 
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
